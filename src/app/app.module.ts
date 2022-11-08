4d486483906390205402e25f93c4e6f27de22d81
Patryk Nowak <patryk.nowak.2012@gmail.com>
2022-11-08 17:07:33 +0100

    added content template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TabComponent } from './components/tab/tab.component';
+import { ContentComponent } from './components/content/content.component';
+import { TextboxComponent } from './components/textbox/textbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    TabComponent
+    TabComponent,
+    ContentComponent,
+    TextboxComponent
   ],
   imports: [
     BrowserModule,
