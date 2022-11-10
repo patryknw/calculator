9b674ff79616a69a97bfa83ced6e9207f589a8b6
Patryk Nowak <patryk.nowak.2012@gmail.com>
2022-11-10 22:59:43 +0100

    pre-emitter state

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -12,4 +12,24 @@ export class ContentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  switchTab(){
+    /*
+    if(tab == tab1){
+      tab1.display = "block";
+      tab2.display = "none";
+      tab3.display = "none";
+    }
+    else if(tab == tab2){
+      tab1.display = "none";
+      tab2.display = "block";
+      tab3.display = "none";
+    }
+    else if(tab == tab3){
+      tab1.display = "none";
+      tab2.display = "none";
+      tab3.display = "block";
+    }
+    */
+  }
+
 }
--- src/app/components/tab/tab.component.ts
+++ src/app/components/tab/tab.component.ts
@@ -9,11 +9,25 @@ export class TabComponent implements OnInit {
   @Input() text!: string;
   @Input() highlighted!: boolean;
   @Input() color!: string;
+  @Input() tab!: string;
 
   constructor() { }
 
   ngOnInit(): void {
     
+  }
+
+  setActive(){
+    this.highlighted = true;
+  }
+
+  setInactive(){
+    this.highlighted = false;
+  }
+
+  onClick(){
+    console.log(this.tab);
+    if(!this.highlighted) this.setActive();
   }
 
 }
